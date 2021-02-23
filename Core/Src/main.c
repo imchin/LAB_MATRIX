45a9ade74106611dcfb180527770b4d302c22121
imchin <54148894+imchin@users.noreply.github.com>
2021-02-24 03:45:27 +0700

    bug variables

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -54,8 +54,10 @@ static void MX_USART2_UART_Init(void);
 //scan and update of button matrix
 uint16_t ButtonMatrixState = 0;
 uint32_t ButtonMatrixTimestamp = 0;
+uint32_t numnow = 0;
 void ButtonMatrixUpdate();
-int checknumpress();
+void checknumpress();
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -64,215 +66,411 @@ int checknumpress();
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
-int main(void) {
-	/* USER CODE BEGIN 1 */
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
 
-	/* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_USART2_UART_Init();
-	/* USER CODE BEGIN 2 */
-	uint16_t numnow = 255;
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
 	uint8_t state = 0;
 	uint16_t ButtonMatrixstatepre=0;
+	uint32_t timestampsampling=0;
+	uint16_t ButtonMatrixState2=0;
+	uint16_t numnow2=0;
 	enum {
-		zero = 0,one,two,three,four,five,six,saven,egg,nine,ten,even,waitclear,clear = 10,BS = 11,ok = 12
+		one=0,two,three,four,five,six,saven,egg,nine,ten,even,twelve,waitclear,clear = 10,BS = 11,ok = 12
 	};
-	/* USER CODE END 2 */
+  /* USER CODE END 2 */
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 	while (1) {
 
 		ButtonMatrixUpdate();
-		numnow = checknumpress();
+		checknumpress();
+		ButtonMatrixState2=ButtonMatrixState;
+		numnow2=numnow;
 		switch (state) {
-			case zero:
+			case waitclear:
+				if ((ButtonMatrixState != 0) && (ButtonMatrixstatepre!=ButtonMatrixState)){
+					if (numnow == clear) {
+						state = one;
+					}
+				}
+			break;
+ 			case one:
 				if ((ButtonMatrixState != 0) && (ButtonMatrixstatepre!=ButtonMatrixState)){
 					if (numnow == 6) {
 						state = two;
 					}else if (numnow == clear){
-						state = zero;
+						state = one;
 					}else if (numnow == BS){
-						state = zero;
+						state = one;
 					}else if (numnow == ok){
-						state = zero;
+						state = one;
+					}else{
+						state=waitclear;
+					}
+				}
+			break;
+ 			case two:
+				if ((ButtonMatrixState != 0) && (ButtonMatrixstatepre!=ButtonMatrixState)){
+					if (numnow == 2) {
+						state = three;
+					}else if (numnow == clear){
+						state = one;
+					}else if (numnow == BS){
+						state = one;
+					}else if (numnow == ok){
+						state = one;
+					}else{
+						state=waitclear;
+					}
+				}
+ 			break;
+ 			case three:
+				if ((ButtonMatrixState != 0) && (ButtonMatrixstatepre!=ButtonMatrixState)){
+					if (numnow == 3) {
+						state = four;
+					}else if (numnow == clear){
+						state = one;
+					}else if (numnow == BS){
+						state = two;
+					}else if (numnow == ok){
+						state = one;
+					}else{
+						state=waitclear;
+					}
+				}
+ 			break;
+ 			case four:
+				if ((ButtonMatrixState != 0) && (ButtonMatrixstatepre!=ButtonMatrixState)){
+					if (numnow == 4) {
+						state = five;
+					}else if (numnow == clear){
+						state = one;
+					}else if (numnow == BS){
+						state = three;
+					}else if (numnow == ok){
+						state = one;
+					}else{
+						state=waitclear;
+					}
+				}
+			break;
+ 			case five:
+				if ((ButtonMatrixState != 0) && (ButtonMatrixstatepre!=ButtonMatrixState)){
+					if (numnow == 0) {
+						state = six;
+					}else if (numnow == clear){
+						state = one;
+					}else if (numnow == BS){
+						state = four;
+					}else if (numnow == ok){
+						state = one;
+					}else{
+						state=waitclear;
+					}
+				}
+ 			break;
+ 			case six:
+				if ((ButtonMatrixState != 0) && (ButtonMatrixstatepre!=ButtonMatrixState)){
+					if (numnow == 5) {
+						state = saven;
+					}else if (numnow == clear){
+						state = one;
+					}else if (numnow == BS){
+						state = five;
+					}else if (numnow == ok){
+						state = one;
+					}else{
+						state=waitclear;
+					}
+				}
+ 			break;
+ 			case saven:
+				if ((ButtonMatrixState != 0) && (ButtonMatrixstatepre!=ButtonMatrixState)){
+					if (numnow == 0) {
+						state = egg;
+					}else if (numnow == clear){
+						state = one;
+					}else if (numnow == BS){
+						state = six;
+					}else if (numnow == ok){
+						state = one;
+					}else{
+						state=waitclear;
+					}
+				}
+ 			break;
+ 			case egg:
+				if ((ButtonMatrixState != 0) && (ButtonMatrixstatepre!=ButtonMatrixState)){
+					if (numnow == 0) {
+						state = nine;
+					}else if (numnow == clear){
+						state = one;
+					}else if (numnow == BS){
+						state = saven;
+					}else if (numnow == ok){
+						state = one;
+					}else{
+						state=waitclear;
+					}
+				}
+ 			break;
+			case nine:
+				if ((ButtonMatrixState != 0) && (ButtonMatrixstatepre!=ButtonMatrixState)){
+					if (numnow == 0) {
+						state = ten;
+					}else if (numnow == clear){
+						state = one;
+					}else if (numnow == BS){
+						state = egg;
+					}else if (numnow == ok){
+						state = one;
+					}else{
+						state=waitclear;
+					}
+				}
+ 			break;
+			case ten:
+				if ((ButtonMatrixState != 0) && (ButtonMatrixstatepre!=ButtonMatrixState)){
+					if (numnow == 2) {
+						state = even;
+					}else if (numnow == clear){
+						state = one;
+					}else if (numnow == BS){
+						state = nine;
+					}else if (numnow == ok){
+						state = one;
+					}else{
+						state=waitclear;
+					}
+				}
+			break;
+			case even:
+				if ((ButtonMatrixState != 0) && (ButtonMatrixstatepre!=ButtonMatrixState)){
+					if (numnow == 6) {
+						state = twelve;
+					}else if (numnow == clear){
+						state = one;
+					}else if (numnow == BS){
+						state = ten;
+					}else if (numnow == ok){
+						state = one;
+					}else{
+						state=waitclear;
+					}
+				}
+			break;
+			case twelve:
+				if ((ButtonMatrixState != 0) && (ButtonMatrixstatepre!=ButtonMatrixState)){
+					if (numnow == clear){
+						state = one;
+					}else if (numnow == BS){
+						state = even;
+					}else if (numnow == ok){
+						HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, RESET);
+						state = one;
+					}else{
+						state=waitclear;
 					}
 				}
 			break;
 		}
 		ButtonMatrixstatepre=ButtonMatrixState;
 
-		/* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-		/* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
 	}
-	/* USER CODE END 3 */
+
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
-void SystemClock_Config(void) {
-	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-	/** Configure the main internal regulator output voltage
-	 */
-	__HAL_RCC_PWR_CLK_ENABLE();
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-	RCC_OscInitStruct.PLL.PLLM = 16;
-	RCC_OscInitStruct.PLL.PLLN = 336;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
-	RCC_OscInitStruct.PLL.PLLQ = 4;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-		Error_Handler();
-	}
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /**
- * @brief USART2 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_USART2_UART_Init(void) {
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
 
-	/* USER CODE BEGIN USART2_Init 0 */
+  /* USER CODE BEGIN USART2_Init 0 */
 
-	/* USER CODE END USART2_Init 0 */
+  /* USER CODE END USART2_Init 0 */
 
-	/* USER CODE BEGIN USART2_Init 1 */
+  /* USER CODE BEGIN USART2_Init 1 */
 
-	/* USER CODE END USART2_Init 1 */
-	huart2.Instance = USART2;
-	huart2.Init.BaudRate = 115200;
-	huart2.Init.WordLength = UART_WORDLENGTH_8B;
-	huart2.Init.StopBits = UART_STOPBITS_1;
-	huart2.Init.Parity = UART_PARITY_NONE;
-	huart2.Init.Mode = UART_MODE_TX_RX;
-	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-	huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-	if (HAL_UART_Init(&huart2) != HAL_OK) {
-		Error_Handler();
-	}
-	/* USER CODE BEGIN USART2_Init 2 */
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
 
-	/* USER CODE END USART2_Init 2 */
+  /* USER CODE END USART2_Init 2 */
 
 }
 
 /**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
-static void MX_GPIO_Init(void) {
-	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-	/* GPIO Ports Clock Enable */
-	__HAL_RCC_GPIOC_CLK_ENABLE();
-	__HAL_RCC_GPIOH_CLK_ENABLE();
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	__HAL_RCC_GPIOB_CLK_ENABLE();
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
-	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
-	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7 | GPIO_PIN_9, GPIO_PIN_SET);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7|GPIO_PIN_9, GPIO_PIN_SET);
 
-	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
 
-	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
 
-	/*Configure GPIO pin : B1_Pin */
-	GPIO_InitStruct.Pin = B1_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
 
-	/*Configure GPIO pin : LD2_Pin */
-	GPIO_InitStruct.Pin = LD2_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-	/*Configure GPIO pins : PA7 PA9 */
-	GPIO_InitStruct.Pin = GPIO_PIN_7 | GPIO_PIN_9;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
-	/*Configure GPIO pin : PC7 */
-	GPIO_InitStruct.Pin = GPIO_PIN_7;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  /*Configure GPIO pins : PA7 PA9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-	/*Configure GPIO pin : PA10 */
-	GPIO_InitStruct.Pin = GPIO_PIN_10;
-	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-	GPIO_InitStruct.Pull = GPIO_PULLUP;
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  /*Configure GPIO pin : PC7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-	/*Configure GPIO pins : PB3 PB4 PB5 */
-	GPIO_InitStruct.Pin = GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5;
-	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-	GPIO_InitStruct.Pull = GPIO_PULLUP;
-	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  /*Configure GPIO pin : PC8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-	/*Configure GPIO pin : PB6 */
-	GPIO_InitStruct.Pin = GPIO_PIN_6;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  /*Configure GPIO pin : PA10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB3 PB4 PB5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
@@ -310,50 +508,53 @@ void ButtonMatrixUpdate() {
 	}
 
 }
-int checknumpress() {
+void checknumpress() {
 	if (ButtonMatrixState == 0b1000000000000) {
-		return 0;
+		numnow=0;
 	} else if (ButtonMatrixState == 0b100000000) {
-		return 1;
+		numnow=1;
 	} else if (ButtonMatrixState == 0b1000000000) {
-		return 2;
+		numnow=2;
 	} else if (ButtonMatrixState == 0b10000000000) {
-		return 3;
+		numnow= 3;
 	} else if (ButtonMatrixState == 0b10000) {
-		return 4;
+		numnow= 4;
+	} else if (ButtonMatrixState == 0b100000) {
+		numnow= 5;
 	} else if (ButtonMatrixState == 0b1000000) {
-		return 5;
-	} else if (ButtonMatrixState == 0b1000000) {
-		return 6;
+		numnow= 6;
 	} else if (ButtonMatrixState == 0b1) {
-		return 7;
+		numnow=7;
 	} else if (ButtonMatrixState == 0b10) {
-		return 8;
+		numnow=8;
 	} else if (ButtonMatrixState == 0b100) {
-		return 9;
+		numnow=9;
 	} else if (ButtonMatrixState == 0b1000) {
-		return 10;
+		numnow=10;
 	} else if (ButtonMatrixState == 0b10000000) {
-		return 11;
+		numnow=11;
 	} else if (ButtonMatrixState == 0b1000000000000000) {
-		return 12;
+		numnow= 12;
 	} else {
-		return 255;
+		numnow=255;
 	}
 }
+
+
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
-void Error_Handler(void) {
-	/* USER CODE BEGIN Error_Handler_Debug */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1) {
 	}
-	/* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
